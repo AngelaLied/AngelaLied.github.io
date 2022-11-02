3e81700909de8cc2c52df8041d4375f8b52a293f
AngelaLieders <lieders.a.v@gmail.com>
2022-11-02 12:39:17 +0300

    изменила кредо

--- js.js
+++ js.js
@@ -3,5 +3,5 @@ const button = document.querySelector('button');
 
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
 button.addEventListener('click', function() {
-	alert('Здесь могла быть твоя реклама')
+	alert('Сегодня самый лучший день')
 })
